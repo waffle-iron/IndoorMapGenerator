d966dd99eecab45b008f3a3f436c14a8daf3b466
ciemek <sharaquss@gmail.com>
2016-07-12 12:20:48 +0200

    1 step

---
+++ Assets/Scripts/IndoorMapGeneratorScript.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using UnityEngine;
+
+public class IndoorMapGeneratorScript : MonoBehaviour
+{
+	//inputs:
+	public float 		metersInOneUnit = 1;
+	public int 			gridSizeX = 10;
+	public int 			gridSizeZ = 10;
+
+	//prefabs:
+	public GameObject 	floorPlane;
+	private Vector3 	floorPlaneOriginalScale;
+	public GameObject 	gridCellPlane;
+	private Vector3 	gridCellPlaneOriginalScale;
+	public GameObject 	gridCellVertex;
+	private Vector3 	gridCellVertexOriginalScale;
+
+	//instantiated GameObjects:
+	private GameObject 	childHolderObject;
+	private GameObject 	floorPlaneObject;
+
+	private String className;
+	private String methodName;
+
+
+	public IndoorMapGeneratorScript()
+	{
+		Debug.Log("IndoorMapGenScript: constructor", this);
+		Utils.Initialize(gridSizeX, gridSizeZ, metersInOneUnit);
+		className = this.GetType().Name;
+//		floorPlaneOriginalScale = Utils.VECTOR_INVALID_VALUE;
+	}
+
+	void Start()
+	{
+		Debug.Log(className + ": " + System.Reflection.MethodBase.GetCurrentMethod().Name, this);
+		Utils.Initialize(gridSizeX, gridSizeZ, metersInOneUnit);
+	}
+
+	public void Update()
+	{
+	}
+
+	public void OnInputUpdate()
+	{
+		Debug.Log(className + ": " + System.Reflection.MethodBase.GetCurrentMethod().Name, this);
+		Utils.UpdateConstants(gridSizeX, gridSizeZ, metersInOneUnit);
+	}
+
+	public void CreateFloorPlane()
+	{
+		Debug.Log(className + ": " + System.Reflection.MethodBase.GetCurrentMethod().Name, this);
+
+		floorPlaneObject = (GameObject) Instantiate(floorPlane);
+		floorPlaneObject.transform.position = Utils.GetOriginCornerXz(Vector3.zero, floorPlaneObject);
+
+		Vector3 size = Utils.GetGridRealSized3D(Utils.PLANE_SIZE_CORRECTION_MULTIPLIER);
+		Debug.LogError("gridreal vec: " + size);
+
+		floorPlaneObject.transform.localScale = size;
+		floorPlaneObject.transform.parent = this.transform;
+		floorPlaneObject.transform.rotation = Quaternion.Euler(0, 0, 0);
+		floorPlaneObject.name = "Floor (" + gridSizeX + ", " + gridSizeZ + ")";
+	}
+
+
+	public void CreateCellularAutomataBoxes()
+	{
+//		Debug.Log(className + ": " + System.Reflection.MethodBase.GetCurrentMethod().Name, this);
+//		GameObject spawned;
+//		Vector3 originalScale = gridCellPlane.transform.localScale;
+//		gridCellPlane.transform.localScale = new Vector3(
+//			originalScale.x/(float)gridSizeX,
+//			originalScale.y,
+//			originalScale.z/(float)gridSizeZ);
+//
+//		Vector3 spawnPosition = Vector3.zero;
+//		for (int x= 0; x < gridSizeX; ++x)
+//		{
+//			for (int z = 0; z < gridSizeZ; ++z)
+//			{
+//				spawned = (GameObject) Instantiate(gridCellPlane, spawnPosition, Quaternion.identity);
+//				spawned.name = "[" + x + ", " + z + "]";
+//				spawned.transform.parent = childHolderObject.transform;
+//				spawnPosition.z += 1 / (float)gridSizeZ;
+//			}
+//
+//			spawnPosition.z = 0f;
+//			spawnPosition.x += 1 / (float)gridSizeX;
+//		}
+	}
+
+//	public void CreateCellularAutomataVertices()
+//	{
+//		Debug.Log(className + ": " + System.Reflection.MethodBase.GetCurrentMethod().Name, this);
+//
+//		GameObject spawned;
+//		if (gridCellVertexOriginalScale == null)
+//		{
+//			gridCellVertexOriginalScale = gridCellVertex.transform.localScale;
+//		}
+//
+//		gridCellVertex.transform.localScale = new Vector3(
+//
+//		);
+////		gridCellPlane.transform.localScale = new Vector3(
+////			originalScale.x/(float)gridSizeX,
+////			originalScale.y,
+////			originalScale.z/(float)gridSizeZ);
+//
+//		Vector3 spawnPosition = Vector3.zero;
+//		for (int x= 0; x < gridSizeX; ++x)
+//		{
+//			for (int z = 0; z < gridSizeZ; ++z)
+//			{
+//				spawned = (GameObject) Instantiate(gridCellPlane, spawnPosition, Quaternion.identity);
+//				spawned.name = "[" + x + ", " + z + "]";
+//				spawned.transform.parent = childHolderObject.transform;
+//				spawnPosition.z += 1 / (float)gridSizeZ;
+//			}
+//
+//			spawnPosition.z = 0f;
+//			spawnPosition.x += 1 / (float)gridSizeX;
+//		}
+//	}
+
+	public void CreateGridVertices()
+	{
+		Debug.Log(className + ": " + System.Reflection.MethodBase.GetCurrentMethod().Name, this);
+	}
+
+
+	public void ClearObjects()
+	{
+		Debug.Log(className + ": " + System.Reflection.MethodBase.GetCurrentMethod().Name, this);
+		if (floorPlaneObject != null)
+		{
+			DestroyFloorPlane();
+		}
+
+		if (childHolderObject == null)
+		{
+			CreateHolder(); //should it be here?
+		}
+		else
+		{
+			DestroyChildren();
+		}
+
+	}
+
+	private void CreateHolder()
+	{
+		Debug.Log(className + ": " + System.Reflection.MethodBase.GetCurrentMethod().Name, this);
+		childHolderObject = new GameObject("level");
+		childHolderObject.transform.parent = this.transform;
+	}
+
+
+	private void DestroyChildren()
+	{
+		Utils.DestroyAssets(childHolderObject);
+	}
+
+	private void DestroyFloorPlane()
+	{
+		Utils.DestroyAsset(floorPlaneObject);
+	}
+
+
+	//input validation method
+	void OnValidate()
+	{
+		if (gridSizeX < 10)
+		{
+			gridSizeX = 10;
+		}
+
+		if (gridSizeZ < 10)
+		{
+			gridSizeZ = 10;
+		}
+	}
+
+
+}
---
+++ Assets/Scripts/Utils.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using UnityEngine;
+
+
+public class Utils
+{
+
+	public static readonly Vector3 	VECTOR_INVALID_VALUE = new Vector3(-1, -1, -1);
+	public static readonly float 	PLANE_SIZE_CORRECTION_MULTIPLIER = 0.1f;
+
+	//VARIABLES:
+	private static float 	metersInOneUnit;
+	private static bool 	initialized = false;
+
+	private static Vector2 	gridSizes;
+	private static Vector2 	gridRealSizes2D;
+	private static Vector3 	gridRealSizes3D;
+
+
+	//CONSTRUCTION AND INITIALIZATION METHODS:
+
+	public static void Initialize(int gridSizeXVal, int gridSizeZVal, float metersInOneUnitVal)
+	{
+		initialized = true;
+		metersInOneUnit = metersInOneUnitVal;
+
+		gridSizes = new Vector2(gridSizeXVal, gridSizeZVal);
+		gridRealSizes2D = new Vector2(GetLength(gridSizeXVal), GetLength(gridSizeZVal));
+		gridRealSizes3D = new Vector3(gridRealSizes2D.x, 1f, gridRealSizes2D.y);
+
+	}
+
+	public static void UpdateConstants(int gridSizeXVal, int gridSizeZVal, float metersInOneUnitVal)
+	{
+		metersInOneUnit = metersInOneUnitVal;
+		gridSizes = new Vector2(gridSizeXVal, gridSizeZVal);
+        gridRealSizes2D = new Vector2(GetLength(gridSizeXVal), GetLength(gridSizeZVal));
+		gridRealSizes3D = new Vector3(gridRealSizes2D.x, 1f, gridRealSizes2D.y);
+	}
+
+
+
+	//UTILITY METHODS:
+
+	public static float GetLength(float valueWithoutMeasurementUnit)
+	{
+		try
+		{
+			return valueWithoutMeasurementUnit * metersInOneUnit;
+		}
+		catch (Exception exception)
+		{
+			if (exception is ArgumentNullException)
+			{
+				Debug.Log("Utils.GetLength(): argument of method was null.");
+			} else if (exception is NullReferenceException)
+			{
+				if (initialized == false)
+				{
+					Debug.Log("Utils.GetLength(): Utils class is not initialized. Dumbass.");
+				}
+				else
+				{
+					Debug.Log("Utils.GetLength(): null in expression (used constant or argument is null).");
+				}
+			}
+		}
+
+		return -1f;
+	}
+
+	public static Vector3 GetOriginCornerXz(Vector3 targetPos, GameObject gameObject)
+	{
+//		return new Vector3(
+//			targetPos.x - gameObject.transform.localScale.x/2f,
+//			targetPos.y,
+//			targetPos.z - gameObject.transform.localScale.z/2f);
+		return new Vector3
+
+	}
+
+	public static Vector3 GetObjectDimensions(GameObject gameObject)
+	{
+		gameObject.GetComponent<MeshFilter>().mesh.bounds.
+
+	}
+
+	public static bool DestroyAsset(GameObject asset)
+	{
+		if (Application.isEditor)
+		{
+			GameObject.DestroyImmediate(asset);
+			return true;
+		}
+
+		if (Application.isPlaying)
+		{
+			GameObject.Destroy(asset);
+			return true;
+		}
+
+		return false;
+	}
+
+	public static bool DestroyAssets(GameObject parent)
+	{
+		bool returnValue = true;
+		foreach (Transform child in parent.transform)
+		{
+			if (DestroyAsset(child.gameObject) == false)
+			{
+				returnValue = false;
+			}
+		}
+
+		return returnValue;
+	}
+
+
+	public static Vector2 getGridRealSizes2D()
+	{
+		return gridRealSizes2D;
+	}
+
+	public static Vector3 GetGridRealSized3D()
+	{
+		return gridRealSizes3D;
+	}
+
+	public static Vector3 GetGridRealSized3D(float multiplier)
+	{
+		Vector3 size = gridRealSizes3D;
+		size.x *= multiplier;
+		size.z *= multiplier;
+		return size;
+	}
+
+}
