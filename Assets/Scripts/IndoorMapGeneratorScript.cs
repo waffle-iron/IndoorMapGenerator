4d011bb42dca5f4bd5a04f93b369c374fdff3ae6
ciemek <sharaquss@gmail.com>
2016-07-26 23:55:43 +0200

    cleanup

--- Assets/Scripts/IndoorMapGeneratorScript.cs
+++ Assets/Scripts/IndoorMapGeneratorScript.cs
@@ -119,16 +119,10 @@ public class IndoorMapGeneratorScript : MonoBehaviour
 	{
 		Random random = new Random();
 
-		for (int x = 0; x < gridRegionsArray.GetLength(0); ++x)
-		{
-			for (int z = 0; z < gridRegionsArray.GetLength(1); ++z)
-			{
-				gridRegionsArray[x,z].SetRegionState(false);
-			}
-		}
+		CleanGridRegionsOff();
 
 		//calculate total number of POIs
-		int pointsOfInterestCount = (int)(gridSizeX * gridSizeZ * poiPercentage / 100f);
+		totalPOIs = (int)(gridSizeX * gridSizeZ * poiPercentage / 100f);
 		int[] randomUniqueNums = UtilsMath.GetUniqueRandomNumbers(pointsOfInterestCount, 0, gridRegionsArray.GetLength(0) * gridRegionsArray.GetLength(1), true);
 
 		Debug.Log("nums: " + Utils.PrintList(randomUniqueNums.ToList()));
@@ -224,216 +218,16 @@ public class IndoorMapGeneratorScript : MonoBehaviour
 
 	}
 
-	//this is what is should look like
-//	public void CreateEntryPoint()
-//	{
-//		float 	centerToCornerDistance;
-//		float 	startingPointDistanceToCenter;
-//		int 	vectorAngle;
-//		centerToCornerDistance = Mathf.Pow(gridSizeX / 2, 2) * Mathf.Pow(gridSizeZ / 2, 2);
-//		centerToCornerDistance = Mathf.Sqrt(centerToCornerDistance);
-//
-//		float 	desiredDistanceCrossingPoint = centerToCornerDistance - (centerToCornerDistance * 0.40f);
-//
-//
-//		Random random = new Random();
-//		int randNum = random.Next(0, 100);
-//		if (randNum <= 70)
-//		{
-//			//choose spot from "desired" units segment
-//			startingPointDistanceToCenter = Mathf.Lerp(desiredDistanceCrossingPoint, centerToCornerDistance, Mathf.InverseLerp(0, 100, randNum));
-//		}
-//		else
-//		{
-//			startingPointDistanceToCenter = Mathf.Lerp(0, desiredDistanceCrossingPoint, Mathf.InverseLerp(0, 100, randNum));
-//			++startingPointDistanceToCenter;
-//		}
-//
-//		vectorAngle = 45;
-//		vectorAngle += random.Next(-20, 20);
-//
-//		Debug.LogError("centerToCorner:" + centerToCornerDistance + ", crossPoint:" + desiredDistanceCrossingPoint);
-//		Debug.LogError("randNum:" + randNum + ", startDist:" + startingPointDistanceToCenter);
-//	}
-
-
-
-
-
-
-//	public void CreateCells()
-//	{
-//		Debug.Log(className + ": " + System.Reflection.MethodBase.GetCurrentMethod().Name, this);
-//
-//		regionCellList.Clear();
-//
-//		Vector3 	regionCellLocalScale;
-//		GameObject 	regionCell;
-//		GameObject 	regionCellHolder;
-//		regionCellHolder = new GameObject();
-//		regionCellHolder.transform.parent = this.transform;
-//
-//		regionCellLocalScale = Utils.GetGridRealSize3D(Utils.PLANE_SIZE_CORRECTION_MULTIPLIER);
-//		Debug.LogError("scale 1: " + regionCellLocalScale);
-//		regionCellLocalScale = Utils.divideXZ(regionCellLocalScale, gridSizeX, gridSizeZ);
-//		Debug.LogError("scale 2: " + regionCellLocalScale);
-//		regionCellLocalScale = Utils.divideXZ(regionCellLocalScale, regionDensity);
-//		Debug.LogError("scale 3: " + regionCellLocalScale);
-//
-//
-//		for (int x = 0; x < (gridSizeX * regionDensity); x++)
-//		{
-//			for (int z = 0; z < (gridSizeZ * regionDensity); z++)
-//			{
-//				regionCell = Instantiate(GridCellPlanePrefab);
-//
-//				regionCell.transform.localScale = regionCellLocalScale;
-//				regionCell.transform.position = Utils.GetTopLeftCornerXZ(Utils.GetLength(x), Utils.GetLength(z), regionCell);
-//
-//				regionCell.transform.parent = regionCellHolder.transform;
-//				regionCell.name = "cell (" + x + ", " + z + ")";
-//
-//				regionCellList.AddLast(regionCell);
-//			}
-//		}
-//
-//	}
-
-
-//	public void CreateGridRegions()
-//	{
-//		Debug.Log(className + ": " + System.Reflection.MethodBase.GetCurrentMethod().Name, this);
-//
-//		gridRegionsList.Clear();
-//
-//		levelHolderObject = new GameObject();
-//		levelHolderObject.transform.parent = this.transform;
-//		levelHolderObject.name = "level";
-//
-//		GameObject region;
-//
-//
-//		for (int x = 0; x < gridSizeX; x++)
-//		{
-//			for (int z = 0; z < gridSizeZ; z++)
-//			{
-//				region = new GameObject();
-//				region.AddComponent<GridCell>().SetGridLocation(x, z);
-//				region.transform.position = Utils.GetTopLeftCornerXZ(Utils.GetLength(x), Utils.GetLength(z), region);
-//				region.transform.parent = levelHolderObject.transform;
-//				region.transform.name = "Region [" + x + ", " + z + "]";
-//				gridRegionsList.AddLast(region);
-//			}
-//		}
-//	}
-
-//	public void CreateRegionCells()
-//	{
-//		for (int x = 0; x < gridSizeX; x++) {
-//			for (int z = 0; z < gridSizeZ; z++) {
-//				for (int d = 0; d < regionDensity; d++) {
-//
-//				}
-//			}
-//		}
-//	}
-
-//	public void CreateGridRegionsPlaneCue()
-//	{
-//		Debug.Log(className + ": " + System.Reflection.MethodBase.GetCurrentMethod().Name, this);
-//
-//		foreach (var region in gridRegionsList)
-//		{
-//
-//		}
-//
-//	}
-
-
-	/*
-	public void CreateCellularAutomataBoxes()
+	private void CleanGridRegionsOff()
 	{
-		Debug.Log(className + ": " + System.Reflection.MethodBase.GetCurrentMethod().Name, this);
-
-		GameObject 	spawned;
-		Vector3 	spawnedScale;
-		Vector3 	spawnedPos = Vector3.zero;
-		spawnedScale = Utils.GetGridRealSize3D(Utils.PLANE_SIZE_CORRECTION_MULTIPLIER);
-		spawnedScale = Utils.divideXZ(spawnedScale, gridSizeX, gridSizeZ);
-
-		gridHolderObject = new GameObject();
-		gridHolderObject.transform.parent = this.transform;
-        gridHolderObject.name = "grid";
-
-		for (int x = 0; x < gridSizeX; x++)
+		for (int x = 0; x < gridRegionsArray.GetLength(0); ++x)
 		{
-			for (int z = 0; z < gridSizeZ; z++)
+			for (int z = 0; z < gridRegionsArray.GetLength(1); ++z)
 			{
-				spawned = Instantiate(GridCellPlanePrefab);
-				spawned.transform.localScale = spawnedScale;
-				spawned.transform.position = Utils.GetTopLeftCornerXZ(Utils.GetLength(x), Utils.GetLength(z), spawned);
-				spawned.transform.parent = gridHolderObject.transform;
-				spawned.name = "grid (" + x + ", " + z + ")";
+				gridRegionsArray[x,z].SetRegionState(false);
 			}
 		}
-
 	}
-	*/
-
-//	public void CreateCellularAutomataVertices()
-//	{
-//		Debug.Log(className + ": " + System.Reflection.MethodBase.GetCurrentMethod().Name, this);
-//
-//		GameObject 	spawned;
-//		Vector3 	spawnedScale;
-//		Vector3 	spawnedPos = Vector3.zero;
-//		spawnedScale = Utils.GetGridRealSize3D(Utils.VERTEX_CUE_SIZE_CORRECTION_MULTIPLIER);
-//		spawnedScale = Utils.divideXZ(spawnedScale, gridSizeX, gridSizeZ);
-//
-//		GameObject 	gridHolder = new GameObject();
-//		gridHolder.transform.parent = this.transform;
-//        gridHolder.name = "vertexes";
-//
-//		for (int x = 0; x < gridSizeX; x++)
-//		{
-//			for (int z = 0; z < gridSizeZ; z++)
-//			{
-//				spawned = Instantiate(GridCellVertexPrefab);
-//				spawned.transform.localScale = spawnedScale;
-//
-//				spawned.transform.position = Utils.GETTOPCOSTAMTESTETETERATESTETESTETEST(Utils.GetLength(x), Utils.GetLength(z), spawned);
-//				spawned.transform.parent = gridHolder.transform;
-//				spawned.name = "(" + x + ", " + z + ")";
-//			}
-//		}
-//	}
-
-
-//	public void ClearObjects()
-//	{
-//		Debug.Log(className + ": " + System.Reflection.MethodBase.GetCurrentMethod().Name, this);
-//		if (floorPlaneObject != null)
-//		{
-//			DestroyFloorPlane();
-//		}
-//
-//		if (childHolderObject == null)
-//		{
-//			CreateHolder(); //should it be here?
-//		}
-//		else
-//		{
-//			DestroyChildren();
-//		}
-//
-//	}
-
-//	private void CreateHolder()
-//	{
-//		Debug.Log(className + ": " + System.Reflection.MethodBase.GetCurrentMethod().Name, this);
-//		childHolderObject = new GameObject("level");
-//		childHolderObject.transform.parent = this.transform;
-//	}
 
 
 	private void DestroyChildren()
